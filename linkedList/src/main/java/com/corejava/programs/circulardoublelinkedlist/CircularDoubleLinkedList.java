0126335a190d767f3d440ae536e8633e93fb2f7f
chinmay.venkata <venkata.sabbam@opsramp.com>
2020-06-06 14:35:39 +0530

    #Removal Methods

--- linkedList/src/main/java/com/corejava/programs/circulardoublelinkedlist/CircularDoubleLinkedList.java
+++ linkedList/src/main/java/com/corejava/programs/circulardoublelinkedlist/CircularDoubleLinkedList.java
@@ -1,5 +1,7 @@
 package com.corejava.programs.circulardoublelinkedlist;
 
+import java.util.Scanner;
+
 import com.corejava.List.List;
 
 
@@ -146,18 +148,118 @@ public class CircularDoubleLinkedList<T> implements List<T> {
 
 	@Override
 	public String positionOfElement(T value) {
-		return null;
+		String position = "";
+		int count = 1;
+		Node<T> current = head;
+		do {
+			if(current.data == value) {
+				position = position.concat(String.valueOf(count)).concat(",");
+				position = position.trim();
+			}
+			current = current.next;
+			count = count+1;
+		}while(current != head);
+
+		if(!position.isEmpty()) {
+			position = position.substring(0,position.length()-1);
+		}
+		return position;
 	}
 
 	@Override
 	public void removeByValue(T value) throws Exception {
-		// TODO Auto-generated method stub
-		
+		String position = positionOfElement(value);
+		System.out.println("Position ::: "+position);
+		if(!position.isEmpty()) {
+			String [] positions = position.split(",");
+			if(positions.length > 1) {
+				Scanner input = new Scanner(System.in);	
+				System.out.println("Given element is present in "+position+" positions");
+				System.out.println("ENTER the Select position from above one , if you need to delete in all positions ENTER all");
+				String inputValue = input.nextLine();
+				if(!inputValue.isEmpty() && position.contains(inputValue)) {
+					removeElementAtPosition(Integer.parseInt(inputValue));
+				}else if(inputValue.equalsIgnoreCase("all")) {
+					removeElementAtPosition(Integer.parseInt(positions[0]));
+					for (int i= 1; i< positions.length;i++) {
+						removeElementAtPosition(Integer.parseInt(positions[i])-i);
+					}
+				}else {
+					throw new Exception("Invalid input");
+				}
+			}else {
+				removeElementAtPosition(Integer.parseInt(positions[0]));
+			}
+		}else {
+			throw new Exception("Value is Not there in the list");
+		}	
 	}
 
 	@Override
 	public void removeAll() {
-		// TODO Auto-generated method stub
-		
+		head = null;
+		tail = null;
 	}
+	
+	private void removeElementAtPosition(int position) throws Exception {
+		if(position > size || position <= 0) {
+			throw new Exception("Invalid Position");
+		}
+		if (head == null) {
+			throw new Exception("List is Empty");
+		}else if (position == 1) {
+			Node<T> temp = head.next;
+			temp.previous = tail;
+			head = temp;
+			tail.next = head;
+			size--;
+		}else if(position == size) {
+			Node<T> temp = tail.previous;
+			temp.next = head;
+			tail = temp;
+			head.previous = tail;
+			size--;
+		}else {
+			int count = 1;
+			for(Node<T> current = head; count < position ; current = current.next) {
+				if(count == position-1) {
+					Node<T> temp = current.next.next;
+					temp.previous = current;
+					current.next = temp;
+					size --;
+				}
+				count = count+1;
+			}
+		}
+	}
+	
+	public void removeElementAtReversePosition(int position) throws Exception {
+		if(head == null) {
+			throw new Exception("List is Empty");
+		}else if (position == 1) {
+			Node<T> temp = tail.previous;
+			temp.next = head;
+			tail = temp;
+			head.previous = tail;
+			size--;
+		}else if (position == size) {
+			Node<T> temp = head.next;
+			temp.previous = tail;
+			head = temp;
+			tail.next = head;
+			size --;
+		}else {
+			int count = 1;
+			for(Node<T> current = tail; count < position ; current = current.previous) {
+				if(count == position-1) {
+					Node<T> temp = current.previous.previous;
+					current.previous = temp;
+					temp.next = current;
+					size--;
+				}
+				count = count+1;
+			}
+		}
+	}
+	
 }
--- linkedList/src/main/java/com/corejava/programs/circulardoublelinkedlist/CircularDoubleLinkedListImplementation.java
+++ linkedList/src/main/java/com/corejava/programs/circulardoublelinkedlist/CircularDoubleLinkedListImplementation.java
@@ -29,6 +29,27 @@ public class CircularDoubleLinkedListImplementation {
       System.out.println(CircularDoubleLinkedList.size());
       System.out.println(st1);
       System.out.println(reverse1);
+      System.out.println("------------------------------");
+      CircularDoubleLinkedList.removeByValue(1);
+      CircularDoubleLinkedList.removeByValue(2);
+      CircularDoubleLinkedList.removeByValue(10);
+      String st2 = CircularDoubleLinkedList.traversingList();
+      System.out.println(st2);
+      CircularDoubleLinkedList.removeByValue(3);
+      String st3 = CircularDoubleLinkedList.traversingList();
+      System.out.println(st3);
+      String reverse2 = CircularDoubleLinkedList.traversingListReverse();
+      System.out.println(reverse2);
+      CircularDoubleLinkedList.removeElementAtReversePosition(1);
+      String st5 = CircularDoubleLinkedList.traversingList();
+      System.out.println(st5);
+      String reverse4 = CircularDoubleLinkedList.traversingListReverse();
+      System.out.println(reverse4);
+      CircularDoubleLinkedList.removeElementAtReversePosition(3);
+      String st4 = CircularDoubleLinkedList.traversingList();
+      System.out.println(st4);
+      String reverse3 = CircularDoubleLinkedList.traversingListReverse();
+      System.out.println(reverse3);
 	}
 
 }
